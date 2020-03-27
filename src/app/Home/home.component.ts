9ec2ad1a041bef194064709875d5b4c2def9a17d
naimish_laptop <mail4naimish@gmail.com>
2020-03-27 23:35:10 +0530

    Inital Commit

---
+++ src/app/Home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { error } from 'util';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  home_data:any;
+  constructor(private http:HttpClient) { }
+
+  ngOnInit() {
+    this.getValues();
+  }
+
+  getValues(){
+    this.http.get('http://localhost:30911/api/home').subscribe(response=>{
+      this.home_data=response;
+      console.log("test data :"+this.home_data);
+    },error=>{
+      console.log(error);
+    });
+  }
+
+}
